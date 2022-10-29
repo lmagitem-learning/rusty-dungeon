4efcbf1a21fab4f3f3ffcd81309963d4ae60cbec
Luc Toupense <luc.toupense@itesoft.com>
2022-10-29 18:45:48 +0200

    Drawing an empty map

---
+++ src/map.rs
@@ -0,0 +1,40 @@
+use crate::prelude::*;
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+pub fn map_index(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+
+    pub fn render(&self, context: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let index = map_index(x, y);
+                match self.tiles[index] {
+                    TileType::Floor => {
+                        context.set(x, y, YELLOW, BLACK, to_cp437('.'));
+                    }
+                    TileType::Wall => {
+                        context.set(x, y, GREEN, BLACK, to_cp437('X'));
+                    }
+                }
+            }
+        }
+    }
+}
