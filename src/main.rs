515e54bb08fe404a81cbda57a44fc2734e632e1b
Luc Toupense <luc.toupense@itesoft.com>
2022-11-01 09:16:55 +0100

    Player moves

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod map;
+mod player;
+
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::player::*;
 }
 use prelude::*;
 
 struct State {
     map: Map,
+    player: Player,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            map: Map::new(),
+            player: Player::new(
+                Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+            ),
+        }
+    }
 }
 
 impl GameState for State {
     fn tick(&mut self, context: &mut BTerm) {
         context.cls();
+        self.player.update(context, &self.map);
         self.map.render(context);
+        self.player.render(context);
     }
 }
 
@@ -24,5 +41,5 @@ fn main() -> BError {
         .with_title("Rusty Dungeon")
         .with_fps_cap(30.0)
         .build()?;
-    main_loop(context, State { map: Map::new() })
+    main_loop(context, State::new())
 }
