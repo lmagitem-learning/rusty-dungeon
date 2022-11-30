4488b093d2ce226edc3f7acc708ee756431067b2
lmagitem <lukemagitem@gmail.com>
2022-11-30 08:17:13 +0100

    ECS working great

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod systems;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
+    pub use legion::systems::*;
     pub use legion::world::*;
     pub use legion::*;
     pub const SCREEN_WIDTH: i32 = 80;
@@ -36,6 +37,12 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
+        map_builder
+            .rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
         Self {
